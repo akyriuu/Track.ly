360423d4c0427ddabf6372719cd2f240c5c1fa67
akyriu <mviniciuscarneiro@hotmail.com>
2026-05-11 00:38:13 -0300

    Simplify PrismaClient initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
